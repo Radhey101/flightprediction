72834b149e69db7582f53208082855110f226d93
Radhey Shyam <rshyam@trugreenmail.com>
2025-07-16 14:53:06 +0530

    add required files

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-import numpy as np
-print(np.__version__)
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+app = Flask(__name__)
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
