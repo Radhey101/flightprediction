b6d360abff885e9997b6ed9a889cf315046028b2
Radhey Shyam <rshyam@trugreenmail.com>
2025-12-03 12:13:16 +0530

    first commit

--- main.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
-app = Flask(__name__)
-@app.route('/')
-def index():
-  return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
